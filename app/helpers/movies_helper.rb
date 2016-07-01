4a27509f4bd358653cb0a5d0db1266a3fb84a89f
Hamza Shafiq <hamza.shafiq@square63.com>
2016-07-01 18:14:35 +0500

    Movies:Styled show index edit views

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -4,4 +4,13 @@ module MoviesHelper
     return if title.blank?
     title.titleize
   end
+
+  def button_title(val)
+    if action_name == 'new'
+      "Create #{val}"
+    else
+      "Update #{val}"
+    end
+  end
+
 end
