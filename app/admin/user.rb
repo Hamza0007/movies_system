8b6fa92b8b34e3340d2e34dcb364093231e87420
Hamza Shafiq <hamza.shafiq@square63.com>
2016-07-14 19:10:39 +0500

    ActiveAdmin:Added user page

---
+++ app/admin/user.rb
@@ -0,0 +1,23 @@
+ActiveAdmin.register User do
+
+  permit_params :email, :password
+
+  index do
+    column :email
+    column :encrypted_password
+    column "Picture" do |m|
+      image_tag(m.attachment.image.url(:thumb)) if m.attachment
+    end
+    column :created_at
+    actions
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :email
+      f.input :password
+    end
+    f.actions
+  end
+
+end
