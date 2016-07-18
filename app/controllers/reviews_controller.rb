9cad19be6ce180f568a8e9c8519f86bc124e2513
Hamza Shafiq <hamza.shafiq@square63.com>
2016-07-18 18:20:53 +0500

    Review: Added restriction that only review owner can edit his reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,7 +2,7 @@ class ReviewsController < ApplicationController
   before_action :set_review, only: [:edit, :update, :destroy]
   before_action :set_movie
   before_action :authenticate_user!, only: [:create, :edit, :update, :destroy]
-
+  before_action :require_permission, only: :edit
 
   def edit
   end
@@ -40,5 +40,11 @@ class ReviewsController < ApplicationController
 
   def review_params
     params.require(:review).permit(:user_id, :movie_id, :comment)
+  end
+
+  def require_permission
+    unless current_user == @review.user
+      redirect_to root_path
+    end
   end
 end
