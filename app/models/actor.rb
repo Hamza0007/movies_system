bc85c3749d53cbfe9165b23093a6f01caf9afe82
Hamza Shafiq <hamza.shafiq@square63.com>
2016-07-18 17:45:36 +0500

    Actor:Added dependant destroy with cast in actor model

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -2,6 +2,6 @@ class Actor < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true, length: { maximum: 20 }
   validates :gender, presence: true, length: { maximum: 6 }, inclusion: { in: ['Male', 'Female'] }
 
-  has_many :casts
+  has_many :casts, dependent: :destroy
   has_many :movies, through: :casts
 end
