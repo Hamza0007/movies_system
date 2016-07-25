dc9c0e3883759c1690c8dd4cd9f81494fe4bb053
Hamza Shafiq <hamza.shafiq@square63.com>
2016-07-25 17:17:09 +0500

    User: Added dependant destroy for associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,10 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
-  has_one :attachment, as: :attachable
-  has_many :reported_reviews
-  has_many :reviews
-  has_many :ratings
+  has_one :attachment, as: :attachable, dependent: :destroy
+  has_many :reported_reviews, dependent: :destroy
+  has_many :reviews, dependent: :destroy
+  has_many :ratings, dependent: :destroy
   has_many :favourite_movies, dependent: :destroy
 
   accepts_nested_attributes_for :attachment
