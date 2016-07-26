9bf63b5b6d293233be62b4422ec39e888ce7e9d3
Hamza Shafiq <hamza.shafiq@square63.com>
2016-07-26 11:48:21 +0500

    FavouriteMovie: Updated favourite movies functionality

--- app/controllers/favourite_movies_controller.rb
+++ app/controllers/favourite_movies_controller.rb
@@ -1,18 +1,28 @@
 class FavouriteMoviesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_movie
+  before_action :favourite_movie_already_added? , only: [:create]
 
   def create
-    unless FavouriteMovie.already_added_to_favourites?(current_user.id, params[:movie_id])
-      @movie = Movie.find_by_id(params[:movie_id])
-      @favourite = @movie.favourite_movies.new
-      @favourite.user = current_user
-      if @favourite.save
-        @message = 'Successfully added movie to favorites'
-      else
-        @message = 'Could not add movie to favourites'
-      end
+    favourite = @movie.favourite_movies.new
+    favourite.user = current_user
+    if favourite.save
+      message = 'Successfully added movie to favorites.'
     else
-      @message = 'Movie Already added to favourites'
+      message = 'Could not add movie to favourites.'
     end
+    redirect_to @movie, notice: message
+  end
+
+  private
+
+  def set_movie
+    @movie = Movie.find_by_id(params[:movie_id])
+  end
+
+  def favourite_movie_already_added?
+    return unless FavouriteMovie.already_added_to_favourites?(current_user, params[:movie_id])
+    redirect_to @movie, notice: 'Movie already added to favourites.'
   end
 
 end
