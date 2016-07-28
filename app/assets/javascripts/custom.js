b4375397b1f7c27c43f29ddebab16c5c39f4fc4f
Hamza Shafiq <hamza.shafiq@square63.com>
2016-07-28 18:33:07 +0500

    ThinkingSphinx: Restricted special characters in title and actors

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -31,4 +31,15 @@ $(document).on('ready page:load', function() {
     }
   });
 
+  $('.txt_box').keypress(function (e) {
+    if (e.charCode != 0) {
+      var regex = new RegExp("^[a-zA-Z0-9\\-\\s]+$");
+      var key = String.fromCharCode(!e.charCode ? e.which : e.charCode);
+      if (!regex.test(key)) {
+        e.preventDefault();
+        return false;
+      }
+    }
+  });
+
 });
